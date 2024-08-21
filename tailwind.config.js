1156bc40fef3bc7405ea9b729151c31e36117bb2
thedevbrian1 <brayomwas95@gmail.com>
2024-08-21 13:20:27 +0300

    Done mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-forest-blue': '#314453',
+        'brand-luminous-green': '#dbdd32'
+      }
+    },
   },
   plugins: [],
 }
-
